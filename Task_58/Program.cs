094ebd4d947d318a87b9ae7127b6177e3976be15
Olesya <lesusha1@yandex.ru>
2022-10-03 11:59:58 +0300

    add Task_58

--- Task_58/Program.cs
+++ Task_58/Program.cs
@@ -6,18 +6,6 @@
 // 18 20
 // 15 18
 
-// int[,] matrix1 = new int[,]
-// {
-//     {2, 4},
-//     {3, 2},
-// };
-
-// int[,] matrix2 = new int[,]
-// {
-//     {3, 4},
-//     {3, 3},
-// };
-
 int row1 = EnterInt("Enter row in matrix1: ");
 int col1 = EnterInt("Enter column in matrix1: ");
 int row2 = EnterInt("Enter row in matrix2: ");
@@ -90,16 +78,5 @@ int[,] MatrixMultiplication(int[,] matrix1, int[,] matrix2)
     }
     return matrixProduct;
 }
-
-
-
-
-
-
-
-
+// Для матрицы 2 х 2
 // matrixProduct[i, j] = matrix1[i, k] * matrix2[k, j] + matrix1[i, k] * matrix2[k, j];
-// matrixProduct[0, 0] = matrix1[0, 0] * matrix2[0, 0] + matrix1[0, 1] * matrix2[1, 0];
-// matrixProduct[0, 1] = matrix1[0, 0] * matrix2[0, 1] + matrix1[0, 1] * matrix2[1, 1];
-// matrixProduct[1, 0] = matrix1[1, 0] * matrix2[0, 0] + matrix1[1, 1] * matrix2[1, 0];
-// matrixProduct[1, 1] = matrix1[1, 0] * matrix2[0, 1] + matrix1[1, 1] * matrix2[1, 1];
